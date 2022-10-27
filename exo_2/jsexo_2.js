3550800cc73ccb452d9c32300f0b1d38655fca40
Klervy <Klervy.ledez.pro@hotmail.com>
2022-10-27 16:54:06 +0200

    début étape 4

--- exo_2/jsexo_2.js
+++ exo_2/jsexo_2.js
@@ -1,5 +1,17 @@
-var givenNumber = parseInt (prompt("Donne moi un nombre."))
+var givenNumber = parseInt (prompt("Donne moi un nombre entre 0 & 50."))
 
+function consigne(nombre_prompt) {
+    if (nombre_prompt>0 && nombre_prompt<50){
+        didIWin(givenNumber)
+    }else{
+        do { 
+            alert ("Nope, entre 0 & 50 on a dit !")
+            var givenNumber2 = parseInt (prompt("Donne moi un nombre entre 0 & 50."))
+            consigne (givenNumber2)
+        } while (nombre_prompt>0 && nombre_prompt<50)
+    }
+}
+consigne (givenNumber)
 function didIWin (number){
     if (number==42){
         return true
@@ -16,9 +28,9 @@ function gamePlay (givenNumber){
     else{        
         do { 
             alert ("Nope, rejoue !")
-            var givenNumber2 = parseInt (prompt("Donne moi un nombre."))
-            didIWin (givenNumber2)
-        } while (didIWin (givenNumber2)==false)
+            var givenNumber3 = parseInt (prompt("Donne moi un nombre entre 0 & 50."))
+            didIWin (givenNumber3)
+        } while (didIWin (givenNumber3)==false)
     alert ("Bien joué nerd")
 
     }
