4bc585f35d587323454a9cac0ce92453f94430fc
Klervy <Klervy.ledez.pro@hotmail.com>
2022-10-28 15:06:53 +0200

    etape 4 finie

--- exo_2/jsexo_2.js
+++ exo_2/jsexo_2.js
@@ -1,38 +1,34 @@
-var givenNumber = parseInt (prompt("Donne moi un nombre entre 0 & 50."))
 
-function consigne(nombre_prompt) {
-    if (nombre_prompt>0 && nombre_prompt<50){
+function consigne() {
+    var givenNumber = parseInt (prompt("Joueur 1Donne moi un nombre entre 0 & 50."))
+
+    if (givenNumber>0 && givenNumber<50){
         didIWin(givenNumber)
     }else{
         do { 
             alert ("Nope, entre 0 & 50 on a dit !")
-            var givenNumber2 = parseInt (prompt("Donne moi un nombre entre 0 & 50."))
-            consigne (givenNumber2)
-        } while (nombre_prompt>0 && nombre_prompt<50)
-    }
-}
-consigne (givenNumber)
-function didIWin (number){
-    if (number==42){
-        return true
-    }
-    else {
-        return false
+            givenNumber = parseInt (prompt("Joueur 1 Donne moi un nombre entre 0 & 50 batârd."))
+        } while (givenNumber<0 || givenNumber>50)
+        didIWin(givenNumber)
     }
 }
 
-function gamePlay (givenNumber){
-    if (didIWin(givenNumber)==true){
+
+
+function didIWin (givenNumber){
+    var guessNumber = parseInt (prompt("Joueur 2 Donne moi un nombre entre 0 & 50."))
+   
+    if (givenNumber==guessNumber){
         alert ("Bien joué nerd")
     }
     else{        
         do { 
             alert ("Nope, rejoue !")
-            var givenNumber3 = parseInt (prompt("Donne moi un nombre entre 0 & 50."))
-            didIWin (givenNumber3)
-        } while (didIWin (givenNumber3)==false)
+            guessNumber = parseInt (prompt("Joueur 2 Donne moi un nombre entre 0 & 50."))
+        } while (givenNumber!==guessNumber)
     alert ("Bien joué nerd")
 
     }
 }
-gamePlay(givenNumber)
+
+consigne ()
