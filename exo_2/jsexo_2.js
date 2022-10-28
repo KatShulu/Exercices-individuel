7cb2f74c5b2264286bb3c22efbee62b12d529882
Klervy <Klervy.ledez.pro@hotmail.com>
2022-10-28 16:15:57 +0200

    version polie

--- exo_2/jsexo_2.js
+++ exo_2/jsexo_2.js
@@ -1,13 +1,13 @@
 
 function consigne() {
-    var givenNumber = parseInt (prompt("Joueur 1Donne moi un nombre entre 0 & 50."))
+    var givenNumber = parseInt (prompt("Joueur 1 Donne moi un nombre entre 0 & 50."))
 
     if (givenNumber>0 && givenNumber<50){
         didIWin(givenNumber)
     }else{
         do { 
             alert ("Nope, entre 0 & 50 on a dit !")
-            givenNumber = parseInt (prompt("Joueur 1 Donne moi un nombre entre 0 & 50 batârd."))
+            givenNumber = parseInt (prompt("Joueur 1 Donne moi un nombre entre 0 & 50 s'il te plait."))
         } while (givenNumber<0 || givenNumber>50)
         didIWin(givenNumber)
     }
@@ -19,15 +19,14 @@ function didIWin (givenNumber){
     var guessNumber = parseInt (prompt("Joueur 2 Donne moi un nombre entre 0 & 50."))
    
     if (givenNumber==guessNumber){
-        alert ("Bien joué nerd")
+        alert ("Bien joué.")
     }
     else{        
         do { 
             alert ("Nope, rejoue !")
             guessNumber = parseInt (prompt("Joueur 2 Donne moi un nombre entre 0 & 50."))
         } while (givenNumber!==guessNumber)
-    alert ("Bien joué nerd")
-
+    alert ("Bien joué.")
     }
 }
 
