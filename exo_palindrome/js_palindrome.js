da2fa8eb2ac5440b39f84c744f93c6d574d40cc3
Klervy <112544312+KatShulu@users.noreply.github.com>
2022-11-23 15:41:15 +0100

    étape 2

--- exo_palindrome/js_palindrome.js
+++ exo_palindrome/js_palindrome.js
@@ -2,17 +2,32 @@
 function isValidDate(string){  
     //Le but est de verifier la validité de la date donnée
     let [day, month, year] = string.split('/')                                      //Methode de split qui attribue a chaque variable un ..   ../../..
-    maxDaysInMonth(month,year)                                         //
-    if (day>0 && day<=maxDays && month>0 && month<13 && year>999 && year<9999){
+    maxDaysInMonth(month,year)                                                      //Envoie vers une autre fonction pour recuperer le nombre jour max du mois entré dans maxDays
+    if (day>0 && day<=maxDays && month>0 && month<13 && year>999 && year<9999){     //  Conditions de verif, dont utilisation de maxDays
+        return isPalindrome(day, month, year)                                                          //Valide
+    }else{
+        console.log ("mauvaise date")                                                                 //Non valide
+    }
+}
+
+
+function maxDaysInMonth (monthx,yearx){    
+    //Sert à recuperer le nombre de jours max dans le mois de l'année des parametres                                     
+    maxDays = new Date(yearx, monthx, 0).getDate()                                  //
+    return maxDays 
+}
+
+function reverseString(str) {
+    return str.split("").reverse().join("");
+}
+
+function isPalindrome(day,month,year){
+   // console.log(reverseString(day+month))
+    //console.log(year)
+    if (reverseString(day+month)==year){
         return true
     }else{
         return false
     }
 }
-
-
-function maxDaysInMonth (monthx,yearx){
-    maxDays = new Date(yearx, monthx, 0).getDate()
-    return maxDays 
-}
-console.log(isValidDate("30/11/2022"))
+console.log(isValidDate("21/02/2022"))
