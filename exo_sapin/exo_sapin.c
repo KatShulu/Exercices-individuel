71bfc63561c1672ab32e35886fb1565537819d1c
Klervy <112544312+KatShulu@users.noreply.github.com>
2023-01-06 11:00:37 +0100

    exo en c fini et clean

--- exo_sapin/exo_sapin.c
+++ exo_sapin/exo_sapin.c
@@ -1,7 +1,5 @@
 #include <stdio.h>
 #include <stdlib.h>
-//#include <math.h>
-//#include <string.h>
 #include <time.h>
 #include <windows.h>
 
@@ -9,30 +7,33 @@
 int sapinTop (int inputUser);
 int sapinBody (int inputUser);
 int genStr(int *pointTable);
-int sapinFoot (int inputUser);
+int sapinTrunk (int inputUser);
 int inputSapin();
 
 int main ()
+    //Main is where i call my functions in the right order
 {
     int inputUser = inputSapin();
 
     sapinTop(inputUser);
     sapinBody(inputUser);
-    sapinFoot (inputUser);
+    sapinTrunk (inputUser);
 
     return 0;
 }
 
 int inputSapin()
+//This one is just for asking through the cmd an input from the user
 {
     int inputUser;
-    printf("Entre nombre batard:\n");
+    printf("Entre la longueur du sapin:\n");
     scanf("%d", &inputUser);
 
     return inputUser;
 }
 
 int sapinTop (int inputUser)
+//Draw the star and the first row of the tree whit the inputUser to print it in the right place
 {
     printf("%.*s", inputUser+4 , "                                                                                                                        ");
     printf("+\n");
@@ -44,61 +45,57 @@ int sapinTop (int inputUser)
 }
 
 int sapinBody (int inputUser)
+//Is used to print the body of the tree row by row
 {
-    int compteur = 0;
-    int largeur = 1;
-    int espace = inputUser + 2;
-    int row = 1;
-    int hauteur = 0;
+    int counter =1, row = 1, width = 1;     //Init of multiple counter at 1 because the 1st row is already here
+    int space = inputUser + 2;
 
-    int string_table[100];
-    int *pointTable = &string_table;
+    int string_table[100];                  //init of the [] for the tree filling 
+    int *pointTable = &string_table;        //init pointeur to be able to modify string_table directly in the memory
 
-    while(compteur<inputUser)
+    while(counter<inputUser)
     {
-        printf("%.*s", espace , "                                                                                                                            ");
-        printf("/");
+        printf("%.*s", space , "                                                                                                                            ");
+        printf("/");                        //multiple prints for the begining of the row    /*
         printf("*");
             
-        for (int i=0; i<largeur; i++){
+        for (int i=0; i<width; i++){        //used for fill the tree whit a rnd char each turn
             
-            genStr(pointTable);
-            Sleep(40);
-            printf("%c", *(pointTable+i));
-        }
+            genStr(pointTable);             //call of the rdnGen whith the pointer init earlier
+            Sleep(40);                      //whithout this memory doesn't change fast enought and the print isn't rnd
+            printf("%c",*(pointTable+i));   //trad: print in a char format, the content of the adress in pointTable
+        }                                   // and next turn do this for the following memory adress
+                                            //the %c is important, i need 42 to be read via ASCII
         printf("*");
         printf("\\\n");
-        compteur++;
-        largeur += 2;
-        espace--;
+        counter++;                          //updates of the various counters 
+        width += 2;
+        space--;
         row++;
 
-        if (row==3)
+        if (row==3)                         //modification of the counters for a nice looking tree
         {
-            largeur-=2;
-            espace++;
+            width-=2;
+            space++;
             row=0;
         }
     }
-    return largeur;
+    return width;
 }
-int genStr(int *pointTable)
-{
-    //int string_table[100];
-    int i;
-    int n = 50;
-    time_t t;
-      
-    /* Intializes random number generator*/ 
-    srand((unsigned) time(&t));
 
-    /* Print 5 random numbers from 0 to 5 */
-    for( i = 0 ; i < n ; i++ ) {
-        int rdnNum = ("%d", rand() % 5);
-        switch ( rdnNum )
+int genStr(int *pointTable)
+//Genereate the filling of the tree
+{
+    time_t t;                               //time stamps necessary for the rnd part
+      
+    srand((unsigned) time(&t));             //Intializes random number generator
+
+    for(int i = 0 ; i < 50 ; i++ ) {        //I want 50 char for each gen in the end
+        int rdnNum = ("%d", rand() % 5);    // rndNum 
+        switch ( rdnNum )                   //Switch to customize the randomness
         {
-            case 1:
-                *(pointTable+i) = 42;
+            case 1:                         //if rndNum==1 
+                *(pointTable+i) = 42;       //the content of the adress in pointTable in the array is 42
                 break;
             case 2:
                 *(pointTable+i) = 111;
@@ -113,25 +110,23 @@ int genStr(int *pointTable)
                 *(pointTable+i) = 42;
         }
     }
-    return 0;
+    return 0;                               //i don't need to return something bcos i modify in the memory directly
 }
 
-int sapinFoot (int inputUser)
+int sapinTrunk (int inputUser)
+//to generate the trunk of the tree
 {
-    if (inputUser==1){
-        printf("%.*s", inputUser+4 , "                                                                                                                        ");
+    if (inputUser==1 || inputUser==2){              //bcos they have only one # i do them manualy
+        printf("%.*s", inputUser+4 , "          "); 
         printf("#");
-    }else if (inputUser == 2){
-        printf("%.*s", inputUser+4 , "                                                                                                                        ");
-        printf("#");    
     }else{
     
-        for(int i=2; i<inputUser; i++)
+        for(int i=2; i<inputUser; i++)              //for all the input <2
         {
             printf("%.*s", inputUser+1 , "                                                                                                                        ");
             printf("  ###\n");
 
-            if (i%2==0){
+            if (i%2==0){                            //i jump step in the loop to match the request of the exercices
                 i++;
             }
         }
